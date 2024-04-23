5a626d9bcc4c1edc3c35f47350f7a1b532545213
Khoi <dangkhoi29mta@gmail.com>
2024-04-23 23:44:19 +0700

    [Add] Announcement bar section

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:hydrogen/typescript',
   ],
   rules: {
+    'prettier/prettier': 0,
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/naming-convention': 'off',
     'hydrogen/prefer-image-component': 'off',
