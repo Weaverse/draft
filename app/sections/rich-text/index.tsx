13015bdd78ba58457bd120f34f714a08efce05ea
Khoi <dangkhoi29mta@gmail.com>
2024-07-09 12:54:35 +0700

    [Done] rich text section

--- app/sections/rich-text/index.tsx
+++ app/sections/rich-text/index.tsx
@@ -7,10 +7,19 @@ import type {CSSProperties} from 'react';
 import {forwardRef} from 'react';
 
 type RichTextData = {
+  backgroundColor: string;
+  textColor: string;
+  heading: string;
+  contentAlignment: string;
+  text: string;
+  sectionHeight: string;
+  buttonLabel: string;
+  buttonLink: string;
+  buttonColor: string;
+  openNewTab: boolean;
+  buttonStyle: string;
   paddingTop: number;
   paddingBottom: number;
-  backgroundColor: string;
-  textPosition: string;
   // More type definitions...
 };
 
@@ -21,9 +30,18 @@ type RichTextProps = HydrogenComponentProps<
 
 let RichText = forwardRef<HTMLElement, RichTextProps>((props, ref) => {
   let {
-    paddingTop,
     backgroundColor,
-    textPosition,
+    textColor,
+    heading,
+    contentAlignment,
+    text,
+    sectionHeight,
+    buttonLabel,
+    buttonLink,
+    buttonColor,
+    openNewTab,
+    buttonStyle,
+    paddingTop,
     paddingBottom,
     children,
     ...rest
@@ -33,14 +51,29 @@ let RichText = forwardRef<HTMLElement, RichTextProps>((props, ref) => {
     paddingTop,
     paddingBottom,
     backgroundColor,
-    alignItems: textPosition,
+    color: textColor,
+    height: sectionHeight,
+    alignItems: contentAlignment,
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: textPosition,
+    justifyContent: contentAlignment,
   } as CSSProperties;
+
   return (
     <section ref={ref} {...rest} style={styleSection}>
-      {children}
+      <h1 className="text-3xl font-bold mb-4">{heading}</h1>
+      <div className=" block mb-6">
+        <p className="text-lg">{text}</p>
+      </div>
+      <a
+        style={{backgroundColor: buttonColor, color: textColor}}
+        href={buttonLink}
+        target={openNewTab ? '_blank' : '_self'}
+        rel={openNewTab ? 'noopener noreferrer' : undefined}
+        className="inline-bloc py-2 px-4 rounded "
+      >
+        {buttonLabel}
+      </a>
     </section>
   );
 });
@@ -53,15 +86,33 @@ export let schema: HydrogenComponentSchema = {
   type: 'rich-text',
   title: 'Rich Text',
   // More schema definitions...
-  childTypes: ['heading', 'paragraph'],
   inspector: [
     {
       group: 'Rich Text',
       inputs: [
         {
+          type: 'color',
+          name: 'backgroundColor',
+          label: 'Background color',
+          defaultValue: '#ffffff',
+        },
+        {
+          type: 'color',
+          name: 'textColor',
+          label: 'Text color',
+          defaultValue: '#000000',
+        },
+        {
+          type: 'text',
+          label: 'Heading',
+          name: 'heading',
+          defaultValue: 'Rich Text',
+          placeholder: 'Enter section heading',
+        },
+        {
           type: 'toggle-group',
-          label: 'Text position',
-          name: 'textPosition',
+          label: 'Content alignment',
+          name: 'contentAlignment',
           configs: {
             options: [
               {label: 'Left', value: 'flex-start'},
@@ -72,10 +123,68 @@ export let schema: HydrogenComponentSchema = {
           defaultValue: 'center',
         },
         {
+          type: 'textarea',
+          label: 'Text',
+          name: 'text',
+          defaultValue:
+            'Pair large text with an image to tell a story, explain a detail about your product.',
+          placeholder: 'Enter your text here...',
+        },
+        {
+          type: 'range',
+          label: 'Section Height',
+          name: 'sectionHeight',
+          defaultValue: 200,
+          configs: {
+            min: 0,
+            max: 800,
+            step: 1,
+            unit: 'px',
+          },
+        },
+        {
+          type: 'text',
+          label: 'Button label',
+          name: 'buttonLabel',
+          defaultValue: 'Button',
+          placeholder: 'Enter the button text!',
+        },
+        {
+          type: 'url',
+          label: 'Button link',
+          name: 'buttonLink',
+          defaultValue: 'https://',
+        },
+        {
+          type: 'color',
+          name: 'buttonColor',
+          label: 'Button color',
+          defaultValue: '#ffffff',
+        },
+        {
+          type: 'switch',
+          label: 'Open in new tab',
+          name: 'openNewTab',
+          defaultValue: true,
+        },
+        {
+          type: 'toggle-group',
+          label: 'Button style',
+          name: 'buttonStyle',
+          configs: {
+            options: [
+              {label: '1', value: '1'},
+              {label: '2', value: '2'},
+              {label: '3', value: '3'},
+            ],
+          },
+          defaultValue: '1',
+        },
+        {
           type: 'range',
           label: 'Padding Top',
           name: 'paddingTop',
-          defaultValue: 40,
+          defaultValue: 64,
           configs: {
             min: 0,
             max: 300,
@@ -87,36 +196,17 @@ export let schema: HydrogenComponentSchema = {
           type: 'range',
           label: 'Padding Bottom',
           name: 'paddingBottom',
-          defaultValue: 40,
+          defaultValue: 64,
           configs: {
             min: 0,
-            max: 300,
+            max: 800,
             step: 1,
             unit: 'px',
           },
         },
-        {
-          type: 'color',
-          name: 'backgroundColor',
-          label: 'Background color',
-          defaultValue: '#ffffff',
-        },
       ],
     },
   ],
-  presets: {
-    children: [
-      {
-        type: 'heading',
-        content: 'Talk about your brand',
-      },
-      {
-        type: 'description',
-        content:
-          'Share information about your brand with your customers. Describe a product, make announcements, or welcome customers to your store.',
-      },
-    ],
-  },
 };
 
 export default RichText;
